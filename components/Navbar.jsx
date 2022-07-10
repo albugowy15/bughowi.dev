4007f8e95e89f6b4f062b982157ec45a24f9e0df
albugowy15 <muhammadbughowy@gmail.com>
2022-07-10 09:38:51 +0700

    modify navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,21 +9,27 @@ import {
   AiFillGithub,
   AiFillLinkedin,
   AiFillTwitterSquare,
+  AiOutlineSwitcher,
 } from "react-icons/ai";
 import { MdOutlineAccountCircle, MdOutlineClose } from "react-icons/md";
 import { FaInstagram } from "react-icons/fa";
-import { BsBook } from "react-icons/bs";
+import { BsBook, BsSun, BsMoon } from "react-icons/bs";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const [theme, setTheme] = useState(false);
   return (
     <>
-      <div className="bg-red-300 flex justify-between items-center p-4">
-        <div className="flex items-center justify-start gap-1">
-          <TbBrandNextjs size={35} />
-          <p className="font-bold text-xl">bughowi.dev</p>
+      <div className="flex justify-between items-center py-2 px-4 sticky">
+        <div className="flex items-center justify-start gap-1 cursor-pointer">
+          <Link href="/">
+            <TbBrandNextjs size={35} />
+          </Link>
+          <Link href="/">
+            <p className="font-bold text-xl">bughowi.dev</p>
+          </Link>
         </div>
 
-        <div className="hidden sm:flex">
+        <div className="hidden sm:flex sm:justify-end sm:items-center">
           <ul className="flex justify-around gap-3">
             <li className="">
               <Link href="/">
@@ -42,10 +48,20 @@ const Navbar = () => {
               <Link href="/about">About</Link>
             </li>
           </ul>
+          <div
+            className="rounded-md border border-white p-2 ml-4 cursor-pointer"
+            onClick={() => setTheme(!theme)}
+          >
+            {theme ? <BsMoon /> : <BsSun />}
+          </div>
           <div></div>
         </div>
         <div className="sm:hidden block">
-          <AiOutlineMenu size={20} />
+          <AiOutlineMenu
+            className="cursor-pointer"
+            onClick={() => setNav(!nav)}
+            size={20}
+          />
         </div>
       </div>
       <div
@@ -58,37 +74,90 @@ const Navbar = () => {
         <div className="bg-darkPrimary w-[70%] p-5">
           <div className="flex justify-between items-center mb-5">
             <p className="text-xl font-bold">bughowi.dev</p>
-            <button className="bg-darkSecondary p-3 rounded-full">
+            <button
+              className="bg-darkSecondary p-3 rounded-full"
+              onClick={() => setNav(!nav)}
+            >
               <MdOutlineClose size={20} />
             </button>
           </div>
           <div>
             <div className="flex items-center gap-4 py-2 cursor-pointer">
               <AiOutlineHome size={20} />
-              <p className="text-lg hover:border-b">Home</p>
+              <Link href="/">
+                <p className="text-lg hover:border-b">Home</p>
+              </Link>
             </div>
-            <div className="flex items-center gap-4 py-2">
+            <div className="flex items-center gap-4 py-2 cursor-pointer">
               <BsBook size={20} />
-              <p className="text-lg">Blog</p>
+              <Link href="/blog">
+                <p className="text-lg hover:border-b">Blog</p>
+              </Link>
             </div>
-            <div className="flex items-center gap-4 py-2">
+            <div className="flex items-center gap-4 py-2 cursor-pointer">
               <AiOutlineProject size={20} />
-              <p className="text-lg">Projects</p>
+              <Link href="/projects">
+                <p className="text-lg hover:border-b">Projects</p>
+              </Link>
             </div>
-            <div className="flex items-center gap-4 py-2">
+            <div className="flex items-center gap-4 py-2 cursor-pointer">
               <MdOutlineAccountCircle size={20} />
-              <p className="text-lg">About</p>
+              <Link href="/about">
+                <p className="text-lg hover:border-b">About</p>
+              </Link>
+            </div>
+            <div className="flex items-center justify-between py-2">
+              <div className="flex gap-4 items-center justify-start cursor-pointer">
+                <AiOutlineSwitcher size={20} />
+                <p className="text-lg hover:border-b">Theme</p>
+              </div>
+              <div
+                className="rounded-md border border-white p-1 cursor-pointer"
+                onClick={() => setTheme(!theme)}
+              >
+                {theme ? <BsMoon /> : <BsSun />}
+              </div>
             </div>
           </div>
           <button className="bg-blueAccent py-3 px-10 rounded-lg my-4 w-full tracking-wide font-bold">
             FOLLOW
           </button>
           <div className="flex items-center justify-start gap-4 my-4">
-            <AiFillFacebook size={25} />
-            <AiFillTwitterSquare size={25} />
-            <FaInstagram size={25} />
-            <AiFillGithub size={25} />
-            <AiFillLinkedin size={25} />
+            <a
+              href="https://facebook.com/muhammad.bugowy/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillFacebook size={25} />
+            </a>
+            <a
+              href="https://twitter.com/bughowy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillTwitterSquare size={25} />
+            </a>
+            <a
+              href="https://instagram.com/muhammad_bughowy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram size={25} />
+            </a>
+            <a
+              href="https://github.com/albugowy15"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub size={25} />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/bughowi"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillLinkedin size={25} />
+            </a>
           </div>
         </div>
       </div>
